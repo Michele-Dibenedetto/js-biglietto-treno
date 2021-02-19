473b82a7e64167882be1fa1936822cbd193ec907
michele di benedetto <michele99cl@gmail.com>
2021-02-19 16:34:33 +0100

    esercizio completato

---
+++ js/main.js
@@ -0,0 +1,23 @@
+// eta del passeggero
+var età = prompt("inserire l'eta del passaggero");
+// kilometri da percorrere
+var km = prompt("inserire i kilometri da percorrere");
+// calcolo prezzo del biglietto
+const euroAlKm = 0.21;
+var costoBiglietto =Math.floor(km * euroAlKm);
+console.log(costoBiglietto);
+// sconto biglietto
+var scontoMinorenni = costoBiglietto * 20 / 100;
+var scontoAnziani = costoBiglietto * 40 / 100;
+var bigliettoScontato;
+
+// sconto in base all'eta
+if (età < 18) {
+    bigliettoScontato = costoBiglietto - scontoMinorenni;
+    alert("sei minorenne e hai accesso allo sconto del 20%, il prezzo del tuo biglietto è " + bigliettoScontato + "€")
+} else if (età > 65) {
+    bigliettoScontato = costoBiglietto - scontoAnziani;
+    alert("sei un'anziano/a e hai accesso allo sconto del 40%, il prezzo del tuo biglietto è " + bigliettoScontato + "€")
+} else {
+    alert("il prezzo del tuo biglietto è " + costoBiglietto + "€")
+}
